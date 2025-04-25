52c2a66626e030de2db39726e5d0a681ed368d42
Bradley Jones <pgawestjones@gmail.com>
2025-04-25 10:48:53 -0700

    Pinned version of  grpcio-tools in setup, bumping version number

--- setup.py
+++ setup.py
@@ -174,7 +174,7 @@ setup(
     package_data={"hivemind": ["proto/*", "hivemind_cli/*"]},
     include_package_data=True,
     license="MIT",
-    setup_requires=["grpcio-tools"],
+    setup_requires=["grpcio-tools==1.71.0"],
     install_requires=install_requires,
     extras_require=extras,
     classifiers=[
